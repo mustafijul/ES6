447d9caaa2d919e804c0188b646991e449d3d7d0
mustafijul <mustafijulislam21@gmail.com>
2025-02-05 08:48:05 +0600

    primitive and non primitive operation done

---
+++ Basic.js
@@ -0,0 +1,35 @@
+// Es6 is basically  a new version of javascript that has been released in 2015. It has a lot of new features that make the code more readable and maintainable.
+
+// 1. Let and Const
+// 2. Arrow Functions
+// 3. Template Literals
+// 4. Default Parameters
+// 5. Rest Parameters
+// 6. Spread Operator
+// 7. Destructuring
+// 8. Classes
+// 9. Modules
+// 10. Promises
+// 11. Generators
+// 12. Async/Await
+
+// Primitive data type
+
+// let x = 10;
+// let name = "tamim";
+
+// let z = x;
+// z = 40;
+
+// console.log(z);
+// console.log(x);
+
+// Non Primitive data type save in same memory location
+
+let x = [1, 2, 3, 4, 5];
+
+let y = x;
+y.push(7);
+
+console.log(y);
+console.log(x);
